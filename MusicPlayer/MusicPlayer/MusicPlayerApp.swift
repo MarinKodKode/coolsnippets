dc617c84dc10c2a4c0b7656376537d2398c48df1
Manuel Alejandro Hernandez Marin <mhernandezm@inbursa.com>
2026-03-20 10:10:34 -0600

    [init] - Repo init

---
+++ MusicPlayer/MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Manuel Alejandro Hernandez Marín on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicPlayer/MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by Manuel Alejandro Hernandez Marín on 20/03/26.
+//
+
+import Testing
+@testable import MusicPlayer
+
+struct MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
